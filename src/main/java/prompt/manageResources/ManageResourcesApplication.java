1c1caa3788b80f2bb35024c7c39e8fe0b2f566f0
LEECHANHEE <ch.lee@promtech.co.kr>
2023-11-29 10:53:36 +0900

    initial commit

---
+++ src/main/java/prompt/manageResources/ManageResourcesApplication.java
@@ -0,0 +1,16 @@
+package prompt.manageResources;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+
+//@SpringBootApplication
+//@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
+public class ManageResourcesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManageResourcesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/prompt/manageResources/ManageResourcesApplicationTests.java
@@ -0,0 +1,13 @@
+package prompt.manageResources;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManageResourcesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
